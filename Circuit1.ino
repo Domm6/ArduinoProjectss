c805ee87249b580f3d9d0510ad9559d952ffa2a1
Dominic <dparker206@icloud.com>
2023-02-21 15:42:05 -0800

    adde circuit 2 controlling LED blink speed

---
+++ Circuit1.ino
@@ -0,0 +1,15 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(13, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(13, HIGH);   // set the LED on
+  delay(1000);              // wait for a second
+  digitalWrite(13, LOW);    // set teh LED off
+  delay(1000);              // wait for a second
+
+}
+
